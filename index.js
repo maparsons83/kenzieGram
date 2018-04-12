06c30189556f0d87a9be627f90e441e8fe858f46
Matt Parsons <maparsons83@yahoo.com>
2018-04-12 13:24:38 -0400

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,71 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'public/' })
+const upload = multer({
+    dest: 'public/uploads'
+})
 const port = 3000;
 const app = express();
+const fs = require('fs');
 
 const uploaded_files = [];
+const uploadsPath = './public/uploads';
+const html = {
+    opening: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="style.css">
+            <title>Document</title>
+        </head>
+        <body>
+    `,
+    header: `<h1>Welcome to Kenziegram!</h1>`,
+    uploadForm: `
+        <form method="post" action="http://localhost:3000/" enctype="multipart/form-data">
+            <div>
+                <label for="file">Choose a picture</label>
+                <input type="file" id="file" name="myFile">
+            </div>
+            <div>
+                <button type="submit">Send the file</button>
+            </div>
+        </form>
+    `,
+    images: ``,
+    ending: `
+            <script src="main.js"></script>
+        </body>
+        </html>
+    `,
+}
 
 app.use(express.static('public'));
 
 app.listen(port);
 
-app.post('/upload', upload.single('myFile'), function (req, res, next) {
+app.get('/', (req, res) => {
+    html.images = "";
+    fs.readdir(uploadsPath, function (err, items) {
+        for (let imagePath of items) {
+            html.images += `
+                <img src="uploads/${ imagePath }">
+            `
+        }
+        res.send(generateHTML());
+    });
+});
+
+app.post('/', upload.single('myFile'), function (req, res, next) {
     // req.file is the `myFile` file
     // req.body will hold the text fields, if there were any
+
     console.log("Uploaded: " + req.file.filename);
     uploaded_files.push(req.file.filename);
-    res.end("Uploaded file!");
-  });
+
+    res.redirect("/");
+});
+
+const generateHTML = () => html.opening + html.header + html.uploadForm + html.images + html.ending;
