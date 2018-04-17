9fcc4205fde1cfb462757c7248855e11066323ae
Matt Parsons <maparsons83@yahoo.com>
2018-04-17 14:15:37 -0400

    minor cleanup

--- index.js
+++ index.js
@@ -9,38 +9,38 @@ const fs = require('fs');
 
 const uploaded_files = [];
 const uploadsPath = './public/uploads';
-const html = {
-    opening: `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <link rel="stylesheet" href="style.css">
-            <title>Document</title>
-        </head>
-        <body>
-    `,
-    header: `<div id=welcomeBox><h1>Welcome to Kenziegram!</h1></div?`,
-    uploadForm: `
-        <form method="post" action="http://localhost:3000/" enctype="multipart/form-data">
-            <div>
-                <label for="file">Choose a picture</label>
-                <input type="file" id="file" name="myFile">
-            </div>
-            <div>
-                <button type="submit">Upload Picture</button>
-            </div>
-        </form>
-    `,
-    images: ``,
-    ending: `
-            <script src="main.js"></script>
-        </body>
-        </html>
-    `,
-}
+// const html = {
+//     opening: `
+//         <!DOCTYPE html>
+//         <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//             <link rel="stylesheet" href="style.css">
+//             <title>Document</title>
+//         </head>
+//         <body>
+//     `,
+//     header: `<div id=welcomeBox><h1>Welcome to Kenziegram!</h1></div?`,
+//     uploadForm: `
+//         <form method="post" action="http://localhost:3000/" enctype="multipart/form-data">
+//             <div>
+//                 <label for="file">Choose a picture</label>
+//                 <input type="file" id="file" name="myFile">
+//             </div>
+//             <div>
+//                 <button type="submit">Upload Picture</button>
+//             </div>
+//         </form>
+//     `,
+//     images: ``,
+//     ending: `
+//             <script src="main.js"></script>
+//         </body>
+//         </html>
+//     `,
+// }
 
 app.use(express.static('public'));
 app.set('view engine', 'pug')
